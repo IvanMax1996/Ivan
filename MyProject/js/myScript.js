cf90c102b2a4623f70711a75a7ea28ae64036a2f
IvanMax1996 <93142938+IvanMax1996@users.noreply.github.com>
2021-11-26 23:02:35 +0200

    Файл полностью обновлён!

--- MyProject/js/myScript.js
+++ MyProject/js/myScript.js
@@ -1,132 +1,261 @@
 "use strict"
 
-let name = prompt('Добрый день, пожалуйста представьтесь!');
+let options = {threshold: [1]};
+let observer = new IntersectionObserver(onEntry, options);
+let elements = $('.element-animation');
 
-if (name) {
-	console.log(name, 'добро пожаловать на сайт!');
-	} 
-	else {
-		console.log('Гость, добро пожаловать на сайт!');
+elements.each((i, el) => {
+    observer.observe(el);
+});
+
+function onEntry (entry) {
+    entry.forEach(change => {
+        if (change.isIntersecting) {
+            change.target.src = change.target.dataset.src;
+        }
+    });
 }
 
-var calculator = {
-	sum: function () {
+////////////////////////////////////////////////////////////////////////////////////////
 
-		let matrix = [];
-		let deadline = [];
-		let type = [
-		['Тип выбранного вами сайта: "Cайт визитка!" Стоимость: 1000 руб! Примерные сроки: 6 дней!',
-		'Тип выбранного вами сайта: "Интернет-магазин!" Стоимость: 1500 руб! Примерные сроки: 12 дней!',
-		'Тип выбранного вами сайта: "Корпоративный сайт!" Стоимость: 2000 руб! Примерные сроки: 19 дней!'],
-		[1000,1500,2000],
-		[6,12,19]
-		];
-
-		let count1 = prompt('Какой вы желаете тип сайта?\nВыберите из предложенного списка:\n0. Сайт-визитка\n1. Интернет-магазин\n2. Корпоративный сайт');
-		
-		if (count1 === null || count1 === "") {
-			matrix[0] = 0;
-			deadline[0] = 0;
-			alert('Вы не ввели нужное число!');
-		}
-		else {
-			if (count1 == 0) {
-				deadline[0] = type[2][0];
-				matrix[0] = type[1][0];		
-				console.log(type[0][0]);
-		}
-		else {
-			if (count1 == 1) {
-				deadline[0] = type[2][1];
-				matrix[0] = type[1][1];
-				console.log(type[0][1]);
-			}
-			else {
-				if (count1 == 2) {
-					deadline[0] = type[2][2];
-					matrix[0] = type[1][2];		
-					console.log(type[0][2]);
-				}
-			}
-		}
-	}
-		
-		let design = [
-		['Стиль выбранного вами дизайна: "Корпоративный!" Стоимость: 1700 руб! Примерные сроки: 10 дней!',
-		'Стиль выбранного вами дизайна: "Рисованный!" Стоимость: 1300 руб! Примерные сроки: 4 дня!',
-		'Стиль выбранного вами дизайна: "Материальный!" Стоимость: 1400 руб! Примерные сроки: 7 дней!'],
-		[1700,1300,1400],
-		[10,4,7]
-		];
-
-		let count2 = prompt('Какой вы желаете дизайн сайта?\nВыберите из предложенного списка:\n0. Корпоративный\n1. Рисованный\n2. Материальный');
-		
-		if (count2 === null || count2 === "") {
-			matrix[1] = 0;
-			deadline[1] = 0;
-			alert('Вы не ввели нужное число!');
-		}
-		else {
-		if (count2 == 0) {
-			deadline[1] = design[2][0];
-			matrix[1] = design[1][0];		
-			console.log(design[0][0]);
-		}
-		else {
-			if (count2 == 1) {
-				deadline[1] = design[2][1];
-				matrix[1] = design[1][1];
-				console.log(design[0][1]);
-			}
-			else {
-				if (count2 == 2) {
-					deadline[1] = design[2][2];
-					matrix[1] = design[1][2];
-					console.log(design[0][2]);
-				}
-			}
-		}
-	}
-
-
-		let layout = [
-		['Выбранный вами тип вёрстки: "Адаптивный!" Стоимость: 2200 руб! Примерные сроки: 8 дней!',
-		'Выбранный вами тип вёрстки: "Мобильная!" Стоимость: 1800 руб! Примерные сроки: 4 дня!',
-		'Выбранный вами тип вёрстки: "Pixel Perfect!" Стоимость: 2600 руб! Примерные сроки: 12 дней!'],
-		[2200,1800,2600],
-		[8,4,12]
-		];
-
-		let count3 = prompt('Какой вы хотите тип вёрстки?\nВыберите из предложенного списка:\n0. Адаптивный\n1. Мобильный\n2. Pixel Perfect');
-		
-		if (count3 === null || count3 === "") {
-			matrix[2] = 0;
-			deadline[2] = 0;
-			alert('Вы не ввели нужное число!');
-		}
-		else
-		if (count3 == 0) {
-			deadline[2] = layout[2][0];
-			matrix[2] = layout[1][0];		
-			console.log(layout[0][0]);
-		}
-		else {
-			if (count3 == 1) {
-				deadline[2] = layout[2][1];
-				matrix[2] = layout[1][1];		
-				console.log(layout[0][1]);
-			}
-			else {
-				if (count3 == 2) {
-					deadline[2] = layout[2][2];
-					matrix[2] = layout[1][2];		
-					console.log(layout[0][2]);
-				} 
-			}
-		}
-		console.log('Сроки выполнения работ -', deadline[0] + deadline[1] + deadline[2], 'дня(ей)!');
-		console.log('Общая стоимость наших услуг составит -', matrix[0] + matrix[1] + matrix[2], 'руб!');
-	}	
+    let option1 = {threshold:[1]};
+    let observer1 = new IntersectionObserver(onEntry1, option1);
+    let element1 = $('.element1');
+    
+    element1.each((i, el) => {
+        observer1.observe(el);
+    });
+   
+  function onEntry1 (entry) {
+    entry.forEach(change => {
+        if (change.isIntersecting) {
+            change.target.classList.add('count1');     
+        }
+    });
+    var thisNumber = $('.count1').text();
+    $({numberValue: thisNumber}).animate({numberValue: 120}, {
+        duration: 1500,
+        easing: 'swing',
+        step: function (now) {
+            $('.count1').text(Math.ceil(this.numberValue));
+        }
+    });      
 }
 
-calculator.sum();
+////////////////////////////////////////////////////////////////////////////////////////
+
+    let option2 = {threshold:[1]};
+    let observer2 = new IntersectionObserver(onEntry2, option2);
+    let element2 = $('.element2');
+    
+    element2.each((i, el) => {
+        observer2.observe(el);
+    });
+   
+  function onEntry2 (entry) {
+    entry.forEach(change => {
+        if (change.isIntersecting) {
+            change.target.classList.add('count2');     
+        }
+    });
+    var thisNumber = $('.count2').text();
+    $({numberValue: thisNumber}).animate({numberValue: 4600}, {
+        duration: 1500,
+        easing: 'swing',
+        step: function (now) {
+            $('.count2').text(Math.ceil(this.numberValue));
+        }
+    });      
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+    let option3 = {threshold:[1]};
+    let observer3 = new IntersectionObserver(onEntry3, option3);
+    let element3 = $('.element3');
+    
+    element3.each((i, el) => {
+        observer3.observe(el);
+    });
+   
+  function onEntry3 (entry) {
+    entry.forEach(change => {
+        if (change.isIntersecting) {
+            change.target.classList.add('count3');     
+        }
+    });
+    var thisNumber = $('.count3').text();
+    $({numberValue: thisNumber}).animate({numberValue: 340}, {
+        duration: 1500,
+        easing: 'swing',
+        step: function (now) {
+            $('.count3').text(Math.ceil(this.numberValue));
+        }
+    });      
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+    let option4 = {threshold:[1]};
+    let observer4 = new IntersectionObserver(onEntry4, option4);
+    let element4 = $('.element4');
+    
+    element4.each((i, el) => {
+        observer4.observe(el);
+    });
+   
+  function onEntry4 (entry) {
+    entry.forEach(change => {
+        if (change.isIntersecting) {
+            change.target.classList.add('count4');     
+        }
+    });
+    var thisNumber = $('.count4').text();
+    $({numberValue: thisNumber}).animate({numberValue: 23}, {
+        duration: 1500,
+        easing: 'swing',
+        step: function (now) {
+            $('.count4').text(Math.ceil(this.numberValue));
+        }
+    });      
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+let option5 = {threshold: [1]};
+let observer5 = new IntersectionObserver(onEntry5, option5);
+let element5 = $('.element5');
+
+element5.each((i, el) => {
+    observer5.observe(el);
+});
+
+function onEntry5 (entry) {
+    entry.forEach(change => {
+        if (change.isIntersecting) {
+            function one () {
+                $('.element5').addClass('animate__animated animate__backInDown show');
+            }
+            function two () {
+                $('.element6').addClass('animate__animated animate__backInRight show');
+            }
+            function three () {
+                $('.element7').addClass('animate__animated animate__backInUp show');
+            }
+            setTimeout(one, 0);
+            setTimeout(two, 700);
+            setTimeout(three, 1400);
+        }
+    });
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+$('#button').click(function(){
+
+        let val1 = $('#list1').val();
+        let val2 = $('#list2').val();
+        let val3 = $('#list3').val();
+        let amount = [];
+        let deadline = [];
+        let type = [
+        [1000,1500,2000],
+        [6,12,19]
+        ];
+
+        if (val1 == 1) {
+
+            deadline[0] = type[1][0];
+            amount[0] = type[0][0];
+        }
+        else if (val1 == 2) {
+            deadline[0] = type[1][1];
+            amount[0] = type[0][1]; 
+        }
+        else if (val1 == 3) {
+            deadline[0] = type[1][2];
+            amount[0] = type[0][2];
+        }
+        else {
+            amount[0] = 0;
+            deadline[0] = 0;
+        }
+
+        let design = [
+        [1700,1300,1400],
+        [10,4,7]
+        ];
+
+        if (val2 == 1) {
+            deadline[1] = design[1][0];
+            amount[1] = design[0][0];
+        }
+        else if (val2 == 2) {
+            deadline[1] = design[1][1];
+            amount[1] = design[0][1]; 
+        }
+        else if (val2 == 3) {
+            deadline[1] = design[1][2];
+            amount[1] = design[0][2];
+        }
+        else {
+            amount[1] = 0;
+            deadline[1] = 0;
+        }
+
+        let layout = [
+        [2200,1800,2600],
+        [8,4,12]
+        ];
+
+        if (val3 == 1) {
+            deadline[2] = layout[1][0];
+            amount[2] = layout[0][0];
+        }
+        else if (val3 == 2) {
+            deadline[2] = layout[1][1];
+            amount[2] = layout[0][1]; 
+        }
+        else if (val3 == 3) {
+            deadline[2] = layout[1][2];
+            amount[2] = layout[0][2];
+        }
+        else {
+            amount[2] = 0;
+            deadline[2] = 0;
+        }
+
+        $('#term').html(deadline[0] + deadline[1] + deadline[2]);
+        $('#amount').html(amount[0] + amount[1] + amount[2]);
+});
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+$(window).scroll(() => {
+    let scrollDistance = $(window).scrollTop();
+
+    $(".section").each((i, el) => {
+
+        if ($(el).offset().top - $('nav').outerHeight() <= scrollDistance) {
+            $ ("nav a").each((i, el) => {
+                if ($(el).hasClass("activemenu")) {
+                    $(el).removeClass("activemenu");
+                    }
+                });
+            $('nav li:eq(' + i +')').find('a').addClass('activemenu'); 
+        }
+        });
+});
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+$('a[href^="#"]').click(function () {
+    let valHref = $(this).attr("href");
+    $('html, body').animate({scrollTop: $(valHref).offset().top - 60 + "px"});
+});
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+var delay_popup = 5000;
+
+setTimeout("document.getElementById('bg_popup').style.display='block'", delay_popup);
